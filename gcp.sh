9214b95e6b594eb3d05fec1c8e6654da17e8b18e
xio3ac <105559525+xio3ac@users.noreply.github.com>
2022-05-15 23:37:23 +0530

    Update gcp.sh

--- gcp.sh
+++ gcp.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig -o us-west.minexmr.com:443 -u DOGE:DG44n1arWRSadrL6x7J8C6CNcR3h4hiazk.gcp11 -k --tls --rig-id GC-US
+./xmrig -o us-west.minexmr.com:443 -u DG44n1arWRSadrL6x7J8C6CNcR3h4hiazk -k --tls --rig-id GC-US
